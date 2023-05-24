503ff2a8aacf69e2b6e69f28903bfbac7289f366
Hossein Mamaghanian <amirhm@gmail.com>
2023-05-24 20:55:24 +0200

    refactor and clean up

--- main.c
+++ main.c
@@ -12,33 +12,14 @@
 #include "st7789.h"
 
 
-#if 0
-int blk_init(){
-	// Tell GPIO blk they are allocated to the PWM
-	gpio_set_function(blk, GPIO_FUNC_PWM);
-
-	// Find out which PWM slice is connected to GPIO 0 (it's slice 0)
-	uint slice_num = pwm_gpio_to_slice_num(blk);
-	pwm_config config = pwm_get_default_config();
-	// Set divider, reduces counter clock to sysclock/this value
-	pwm_config_set_clkdiv(&config, 4.f);
-	// Load the configuration into our PWM slice, and set it running.
-	pwm_init(slice_num, &config, true);
-	return slice_num;
-}
-
-void blk_set_value(int slice_num, int val){
-	// Set period of 4 cycles (0 to 3 inclusive)
-	pwm_set_wrap(slice_num, 3);
-	// Set channel A output high for one cycle before dropping
-	pwm_set_chan_level(slice_num, PWM_CHAN_A, 1);
-	// Set initial B output high for three cycles before dropping
-	pwm_set_chan_level(slice_num, PWM_CHAN_B, 3);
-	// Set the PWM running
-	pwm_set_enabled(slice_num, true);
-}
-#endif
-
+typedef struct SensorData{
+	uint16_t co2_raw;
+	uint16_t temperature_raw;
+	uint16_t humidity_raw;
+	float temperature
+	float humidity;
+} SensorData;
+SensorData weather;
 
 
 int init_i2c(){
@@ -64,60 +45,72 @@ int init_spi(){
 	return 0;
 }
 
-int main(){
+int init_peripherals(){
 	stdio_init_all();
 	init_gpio();
-	// int slice_n = blk_init();
 	init_spi();
 	init_i2c();
+	return 0;
+}
 
-	init_display();
-	stop_measurements();
+int init_sensors(){
 	uint16_t alt = 0;
 	uint32_t serial;
+	uint16_t status;
+	int i = 0;
+	stop_measurements();
 	get_serial_number(&serial);
 	get_sensor_altitude(&alt);
 	set_sensor_altitude(562);
 	get_sensor_altitude(&alt);
-
-	uint16_t co2, temp, rh;
-	float temperature, rhumid;
-	uint16_t status;
-
-	int i = 0;
 	start_measurements();
-	fill_display_gradient();
-	sleep_ms(1000);
+}
+
+
+int sensor_task(){
+	int status = 0;
+	if (get_status_ready())
+	{
+		status = read_measurements(&weather.co2_raw, &weather.temperature_raw, &weather.humidity_raw);
+		display.updated = true;
+		display.update_weather = true;
+	}
+	if(!status){
+		temperature = -45 + (float)(175 * weather.temperature_raw) / (65536.0);
+		rhumid = (float)(100 * weather.humidity_raw) / (65536.0);
+		sleep_ms(10);
+	}
+}
+static int display_info_update(){
+	write_number(display.ML, display.LineHeight, weather.co2);
+	write_number(display.ML, display.LineHeight * 2, (uint16_t) weather.temperature);
+	write_number(display.ML, display.LineHeight * 3, (uint16_t) weather.humidity);
+	display.update_weather = false;
+}
+static int display_task(){
+	if (display.updated){
+		fill_display(color565(0,0,0));
+	}
+	if (display.update_weather){
+		display_info_update();
+	}
+	if (display.updated){
+		update_display();
+		display.update = false;
+	}
+}
+
+int main(){
+	init_peripherals();
+	init_display();
+	init_sensors();
+
 	fill_display(color565(0, 0, 0));
 
 	while (true){
-		if (get_status_ready() )
-		{
-			read_measurements(&co2, &temp, &rh);
-			temperature = -45 + (float)(175 * temp) / (65536.0);
-			rhumid = (float)(100 * temp) / (65536.0);
-			printf("co2 %d temp %f  rh %f\n", co2, temperature, rhumid);
-			sleep_ms(10);
-			i++;
-			fill_display(color565(0,0,0));
-
-			// write_character(100, 150, (i % 10));
-			write_number(10, 50, co2);
-			write_number(10, 150, (uint16_t) temperature);
-			write_number(10, 250, (uint16_t) rhumid);
-			update_display();
-		}
+		sensor_task();
+		display_task();
 	}
-
 	printf("Done.\n");
 	return 0;
 }
-#if 0
-int mains() {
-	stdio_init_all();
-	init_gpio();
-	// int slice_n = blk_init();
-	init_spi();
-	init_i2c();
-}
-#endif
